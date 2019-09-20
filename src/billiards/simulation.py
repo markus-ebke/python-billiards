e4dbb9e108aabef3cd228c10e10c6962d8b81422
Markus Ebke <markus.ebke92@gmail.com>
2019-09-20 16:07:51 +0200

    Implement time of impact calculation
    The time of impact is calculated by a function in billiards.simulation. Added
    function documentation and tests.
    
    Added radius parameter to Simulation.add_balls and balls_radius attribute, will
    be needed later.

--- src/billiards/simulation.py
+++ src/billiards/simulation.py
@@ -1,6 +1,64 @@
 # -*- coding: utf-8 -*-
 """Main code for billiard simulation."""
+from math import sqrt
+
 import numpy as np
+
+INF = float("inf")
+
+
+def time_of_impact(pos1, vel1, radius1, pos2, vel2, radius2):
+    """Calculate the time of impact of two moving balls.
+
+    Args:
+        pos1: Center of the first ball.
+        vel1: Velocity of the first ball.
+        radius1: Radius of the first ball.
+        pos2: Center of the second ball.
+        vel2: Velocity of the second ball.
+        radius2: Radius of the second ball.
+
+    Return:
+        Non-negative time of impact, is infinite if there is no impact
+
+    """
+    # switch to coordinate system of ball 1
+    pos_diff = np.subtract(pos2, pos1)
+    vel_diff = np.subtract(vel2, vel1)
+
+    pos_dot_vel = np.dot(pos_diff, vel_diff)
+    if pos_dot_vel >= 0:
+        # balls are moving apart, no impact
+        return INF
+    else:
+        pos_sqrd = np.dot(pos_diff, pos_diff)
+
+        dist_sqrd = pos_sqrd - (radius1 + radius2) ** 2
+        if dist_sqrd < 0:
+            # one ball is inside the other, but don't count this as an impact
+            return INF
+
+        # time of impact is given as the solution of the quadratic equation
+        # t^2 + b t + c = 0 with b and c below
+        vel_sqrd = np.dot(vel_diff, vel_diff)  # note vel_sqrd != 0
+        assert vel_sqrd > 0, vel_sqrd
+        b = pos_dot_vel / vel_sqrd  # note b < 0
+        assert b < 0, b
+        c = dist_sqrd / vel_sqrd  # note c >= 0
+        assert c >= 0, c
+
+        discriminant = b ** 2 - c
+        if discriminant <= 0:
+            # the balls miss
+            return INF
+
+        # when writing the solution of the quadratic equation use that
+        # c = t1 t2
+        t1 = -b + sqrt(discriminant)  # note t1 > 0
+        assert t1 > 0, (t1, b, sqrt(discriminant))
+        t2 = c / t1  # note t2 >= 0, is zero if c = 0, i.e. balls are touching
+        assert t2 >= 0, (t2, c)
+        return min(t1, t2)
 
 
 class Simulation(object):
@@ -11,6 +69,7 @@ class Simulation(object):
         num: Number of simulated balls.
         balls_position: List of 2D position of the balls' centers.
         balls_velocity: List of 2D velocities of the balls.
+        balls_radius: List of numbers that represent the radii of the balls.
 
     """
 
@@ -21,20 +80,25 @@ class Simulation(object):
 
         self.balls_position = np.empty(shape=(0, 2), dtype=np.float64)
         self.balls_velocity = np.empty(shape=(0, 2), dtype=np.float64)
+        self.balls_radius = []
 
-    def add_ball(self, pos, vel):
+    def add_ball(self, pos, vel, radius=0):
         """Add a ball at the given position with the given velocity.
 
         Args:
             pos: A 2D vector that represents the center of the ball.
             vel: A 2D vector that represents the velocity of the ball.
+            radius (optional): The radius of the added ball.
+                Defaults to 0 in which case the ball behaves like a point
+                particle.
 
         Returns:
-            index: List-index where the balls' information is stored.
+            List-index where the balls' information is stored.
 
         """
         self.balls_position = np.append(self.balls_position, [pos], axis=0)
         self.balls_velocity = np.append(self.balls_velocity, [vel], axis=0)
+        self.balls_radius.append(radius)
 
         self.num = self.balls_position.shape[0]
         idx = self.num - 1  # last added ball is at the end
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,7 +1,53 @@
 # -*- coding: utf-8 -*-
+from math import sqrt
+
 import pytest
 
 from billiards import Simulation
+from billiards.simulation import time_of_impact
+
+
+def test_time_of_impact():
+    inf = float("inf")
+
+    # check that only relative coordinates are important
+    assert time_of_impact((0, 42), (42, 0), 1, (5, 42), (41, 0), 1) == 3
+    assert time_of_impact((0, 0), (0, 0), 1, (5, 0), (-1, 0), 1) == 3
+
+    # for convenience
+    def toi(p2, v2, r2):
+        return time_of_impact((0, 0), (0, 0), 1, p2, v2, r2)
+
+    # check miss
+    assert toi((2, 0), (1, 0), 1) == inf
+    assert toi((2, 0), (0, 1), 1) == inf
+
+    # check head-on impact
+    assert toi((3, 0), (-1, 0), 1) == 1.0
+    assert toi((0, 101), (0, -33), 1) == pytest.approx(3.0)
+
+    # check sliding past each other
+    assert toi((2, 0), (0, 1), 1) == inf
+    assert toi((2, 10), (0, -1), 1) == inf
+
+    # check sideways collision
+    # length of diagonal of a unit square is sqrt(2)
+    assert toi((1, 2), (0, -1), sqrt(2) - 1) == pytest.approx(1)
+
+    # check touching, note that this might not work so nicely with floating
+    # point numbers
+    assert toi((2, 0), (-1, 0), 1) == 0
+    assert toi((1 + 1e-12, 1), (0, -1), sqrt(2) - 1) == pytest.approx(0.0)
+
+    # check one inside the other
+    assert toi((1, 0), (-42, 0), 1) == inf
+    assert toi((1, 0), (-42, 0), 10) == inf
+
+    # check point particle
+    assert toi((2, 0), (-1, 0), 0) == 1  # head-on
+    assert toi((1, 0), (0, 1), 0) == inf  # slide
+    # cos(60°) == 1/2 => pythagoras: sin(60°) == sqrt(1 - 1/2**2) == sqrt(3/4)
+    assert toi((0.5, 1), (0, -1), 0) == pytest.approx(1 - sqrt(3 / 4))  # side
 
 
 def test_index():
